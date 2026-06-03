20546eda8e5465e226e30872daea6e2432d66ba4
Sumant-Shekhar <sumantshekhar.dev@gmail.com>
2026-06-03 21:25:44 +0530

    Implement DRF serializer, API view, and redirection logic

--- shortener/urls.py
+++ shortener/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index'),
+    path('api/shorten/', views.URLCreateAPIView.as_view(), name='api-shorten'),
+    path('<str:short_code>/', views.redirect_view, name='redirect'),
 ]
